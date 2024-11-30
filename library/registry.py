d4e8ea04de3c88146f5b966a39849b021fc7a534
Eran <agmon.eran@gmail.com>
2024-11-30 13:40:52 -0500

    cache allowed obj containment relations and process participation

--- library/registry.py
+++ library/registry.py
@@ -22,9 +22,11 @@ class SchemaRegistry:
         self.object_meta_schema = object_meta_schema
         self.process_meta_schema = process_meta_schema
 
-
         # to save allowed object containments
         self.allowed_containments = {}
+
+        # save process-object participation
+        self.process_participation = {}
 
         # to save type inheritance
         # self.type_inheritance = {}
@@ -40,10 +42,10 @@ class SchemaRegistry:
 
             contained_object_types = schema.get('contained_object_types')
             if contained_object_types:
+                if schema_name not in self.allowed_containments:
+                    self.allowed_containments[schema_name] = []
                 for obj_type in contained_object_types:
-                    if obj_type not in self.allowed_containments:
-                        self.allowed_containments[obj_type] = []
-                    self.allowed_containments[obj_type].append(schema_name)
+                    self.allowed_containments[schema_name].append(obj_type)
 
         except ValidationError as e:
             print(f"Failed to register object schema '{schema_name}': {e.message}")
@@ -53,6 +55,14 @@ class SchemaRegistry:
             self.validate_schema(schema, self.process_meta_schema)
             self.processes[schema_name] = schema
             print(f"Process schema '{schema_name}' registered successfully.")
+
+            participating_objects = schema.get('participating_objects')
+            if participating_objects:
+                if schema_name not in self.process_participation:
+                    self.process_participation[schema_name] = []
+                for obj_type in participating_objects:
+                    self.process_participation[schema_name].append(obj_type)
+
         except ValidationError as e:
             print(f"Failed to register process schema '{schema_name}': {e.message}")
 
@@ -71,3 +81,16 @@ def register_schemas_from_directory(directory, register_function):
                 schema = json.load(schema_file)
                 schema_name = os.path.splitext(filename)[0]
                 register_function(schema_name, schema)
+
+
+if __name__ == '__main__':
+    from schemas import schema_registry
+
+    print("Object schemas:")
+    print(schema_registry.objects)
+    print("Process schemas:")
+    print(schema_registry.processes)
+    print("Allowed containments:")
+    print(schema_registry.allowed_containments)
+    print("Process participation:")
+    print(schema_registry.process_participation)
